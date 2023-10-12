41981c1467ca245d51d2e6b3cf7016207440e796
Levente Hunyadi <levente.hunyadi@instructure.com>
2023-10-12 11:24:31 +0200

    Publish new version to PyPI

--- tsv/__init__.py
+++ tsv/__init__.py
@@ -1,4 +1,12 @@
-__version__ = "0.3"
+"""
+tsv2py: Parse and generate tab-separated values (TSV) data
+
+This package offers a high-performance alternative to convert data between a TSV text file and Python objects.
+The parser can read a TSV record into a Python tuple consisting of built-in Python types, one for each field.
+The generator can produce a TSV record from a tuple.
+"""
+
+__version__ = "0.4"
 __author__ = "Levente Hunyadi"
 __copyright__ = "Copyright 2023, Levente Hunyadi"
 __license__ = "MIT"
