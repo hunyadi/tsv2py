12d62c2204a05ca3b7093f31d6a2d3ce7b7e276a
Levente Hunyadi <levente.hunyadi@instructure.com>
2024-05-31 09:03:48 +0200

    Publish new version to PyPI

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@ set -e
 if [ -d build ]; then rm -rf build; fi
 if [ -d wheelhouse ]; then rm -rf wheelhouse; fi
 if [ -d *.egg-info ]; then rm -rf *.egg-info; fi
-MACOSX_DEPLOYMENT_TARGET=10.9 /usr/bin/python3 -m build --outdir wheelhouse
+MACOSX_DEPLOYMENT_TARGET=10.9 TAR_OPTIONS=--no-xattrs /usr/bin/python3 -m build --outdir wheelhouse
 cibuildwheel --platform linux
 abi3audit wheelhouse/*-cp3*.whl
